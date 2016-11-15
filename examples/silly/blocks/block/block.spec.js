4a438f356e040cee44fab5f751462b865df869ed
Evgeniy Baranov <master@kompolom.ru>
2016-11-15 13:32:20 +0300

    es6 syntax for spec

--- examples/silly/blocks/block/block.spec.js
+++ examples/silly/blocks/block/block.spec.js
@@ -1,7 +1,7 @@
 modules.define('spec', function(provide) {
 
     describe('block-1', function() {
-        it('should be equal to one', function() {
+        it('should be equal to one', () => {
             (1*1).should.to.equal(1);
         });
     });
